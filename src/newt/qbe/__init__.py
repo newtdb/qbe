fdd505e0ad8cbd5e1307862a689ca66bdada2657
Jim Fulton <jim@zope.com>
2017-03-29 17:57:44 -0400

    Finished initial helpers and tests.

--- src/newt/qbe/__init__.py
+++ src/newt/qbe/__init__.py
@@ -1,12 +1,20 @@
 import contextlib
+from newt.db.search import read_only_cursor
+import persistent
 import persistent.mapping
 import re
 import six
 
+
 is_identifier = re.compile(r'\w+$').match
 is_access = re.compile(r"state\s*(->\s*(\d+|'\w+')\s*)+$").match
 
-class scalar:
+class Index(persistent.Persistent):
+
+    def order_by(self, cursor, query):
+        return self.expr.encode('ascii')
+
+class scalar(Index):
 
     def __init__(self, expr, type=''):
         if is_identifier(expr):
@@ -38,7 +46,7 @@ class scalar:
         else:
             return cursor.mogrify(self._range, (min, max))
 
-class array:
+class array(Index):
 
     def __init__(self, expr, type=''):
         if is_identifier(expr):
@@ -60,56 +68,77 @@ class array:
     def __call__(self, cursor, query):
         return cursor.mogrify(self._any, (query,))
 
-class prefix:
+class prefix(Index):
 
     def __init__(self, expr, delimiter=''):
         if is_identifier(expr):
             expr = 'state ->> %r' % expr
-        expr = '(' + expr + ')'
-        self.expr = expr
-
-        self._like = "%s like %%s || '%s%%%%'" % (expr, delimiter)
-
-    def __call__(self, cursor, query):
-        return cursor.mogrify(self._like, query)
-
-class fulltext:
-
-    def __init__(self, expr, config=None, parser=None):
-        if identifier(expr):
-            expr = "state -> " + expr
 
         if is_access(expr):
             expr = '>>'.join(expr.rsplit('>', 1))
-            expr = 'to_tsvector(%s%s)' % (repr(config) + ', ' if config else '',
-                                          expr)
+
+        expr = '(' + expr + ')'
+        self.expr = expr
+
+        self._like = "(%s like %%s || '%s%%%%')" % (expr, delimiter)
+
+    def __call__(self, cursor, query):
+        return cursor.mogrify(self._like, (query,))
+
+class fulltext(Index):
+
+    def __init__(self, expr,
+                 config=None,
+                 parser=None,
+                 weights=(.1, .2, .4, 1.0),
+                 ):
+        if is_identifier(expr):
+            expr = "state -> %r" % expr
+
+        config = repr(config) + ', ' if config else ''
+
+        if is_access(expr):
+            expr = '>>'.join(expr.rsplit('>', 1))
+            expr = 'to_tsvector(%s%s)' % (config, expr)
         else:
             expr = '(' + expr + ')'
 
         self.expr = expr
 
-        self._search = '%s @@ %%s' % expr
+        self._search = "%s @@ to_tsquery(%s%%s)" % (expr, config)
+        d, c, b, a = weights
+        self._order = "ts_rank_cd({%g, %g, %g, %g}, %s, to_tsquery(%s%%s))" % (
+            d, c, b, a, expr, config)
+
+        self.parser = parser
+        self.weights = weights
 
     def __call__(self, cursor, query):
         if self.parser is not None:
             query = self.parser(query)
-        return cursor.mogrify(self._search, query)
+        return cursor.mogrify(self._search, (query,))
+
+    def order_by(self, cursor, query):
+        if self.parser is not None:
+            query = self.parser(query)
+        return cursor.mogrify(self._order, (query,))
 
 class QBE(persistent.mapping.PersistentMapping):
 
     def sql(self, query, order_by=None, desc=False):
-        with contextlib.closing(self._p_jar._storage.ex_cursor()) as cursor:
-            result = [b'SELECT * FROM newt']
+        with contextlib.closing(read_only_cursor(self._p_jar)) as cursor:
+            result = []
             wheres = [self[name](cursor, q) for name, q in query.items()]
             if wheres:
-                result.append(b' WHERE ')
                 result.append(
                     b' AND '.join(
-                        self[name](cursor, q) for name, q in query.items()
+                        self[name](cursor, q)
+                        for name, q in sorted(query.items())
                         )
                     )
             if order_by:
-                result.append(b' ORDER BY ' + self[order_by].expr)
+                result.append(b' ORDER BY ' +
+                              self[order_by].order_by(cursor, query[order_by]))
                 if desc:
                     result.append(b' DESC')
 
--- src/newt/qbe/tests.py
+++ src/newt/qbe/tests.py
@@ -11,8 +11,11 @@ class QBETests(newt.db.tests.base.TestCase):
         self.conn = newt.db.connection(self.dsn)
         self.conn.root.qbe = self.qbe = newt.qbe.QBE()
         self.conn.commit()
+        from newt.db.search import read_only_cursor
+        self.cursor = read_only_cursor(self.conn)
 
     def tearDown(self):
+        self.cursor.close()
         self.conn.close()
         super(QBETests, self).tearDown()
 
@@ -21,72 +24,82 @@ class QBETests(newt.db.tests.base.TestCase):
         self.qbe['x'] = scalar('x')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x') = 'y')",
+            b"((state ->> 'x') = 'y')",
             self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(state ->> 'x')",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
 
         from newt.qbe import scalar
         self.qbe['x'] = scalar("state -> 'x'")
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x') = 'y')",
+            b"((state ->> 'x') = 'y')",
             self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(state ->> 'x')",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
 
         self.qbe['x'] = scalar('x', type='int')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x')::int = 1)",
+            b"((state ->> 'x')::int = 1)",
             self.qbe.sql(dict(x=1)))
+        self.assertEqual(b"(state ->> 'x')::int",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
 
         self.qbe['x'] = scalar("state ->> 'x'")
         self.conn.commit()
-        self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x') = 'y')",
-            self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"((state ->> 'x') = 'y')", self.qbe.sql(dict(x='y')))
 
         self.qbe['x'] = scalar("state ->> 'x'", type='int')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x')::int = 1)",
+            b"((state ->> 'x')::int = 1)",
             self.qbe.sql(dict(x=1)))
 
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x')::int <= 1)",
+            b"((state ->> 'x')::int <= 1)",
             self.qbe.sql(dict(x=(None, 1))))
 
         self.assertEqual(
-            b"SELECT * FROM newt WHERE ((state ->> 'x')::int >= 1)",
+            b"((state ->> 'x')::int >= 1)",
             self.qbe.sql(dict(x=(1, None))))
 
         self.assertEqual(
-            b"SELECT * FROM newt WHERE "
             b"(((state ->> 'x')::int >= 1) and"
             b" ((state ->> 'x')::int <= 2))",
             self.qbe.sql(dict(x=(1,2))))
+        self.assertEqual(b"(state ->> 'x')::int",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
 
     def test_array(self):
         from newt.qbe import array
         self.qbe['x'] = array('x')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE "
             b"array(select value from jsonb_array_elements_text(state -> 'x'))"
             b" && ARRAY['a', 'b', 'c']",
             self.qbe.sql(dict(x=['a', 'b', 'c'])))
+        self.assertEqual(
+            b"array(select value from jsonb_array_elements_text(state -> 'x'))",
+            self.qbe['x'].order_by(self.cursor, None))
 
         self.qbe['x'] = array('x', type='int')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE "
             b"array("
             b"select value::int from jsonb_array_elements_text(state -> 'x')"
             b")"
             b" && ARRAY[0, 1, 2]",
             self.qbe.sql(dict(x=[0,1,2])))
+        self.assertEqual(
+            b"array("
+            b"select value::int from jsonb_array_elements_text(state -> 'x')"
+            b")",
+            self.qbe['x'].order_by(self.cursor, None))
 
         self.qbe["x"] = array("state->0-> 'z' ", type='int')
         self.conn.commit()
         self.assertEqual(
-            b"SELECT * FROM newt WHERE "
             b"array("
             b"select value::int from jsonb_array_elements_text("
             b"state->0-> 'z' "
@@ -94,3 +107,117 @@ class QBETests(newt.db.tests.base.TestCase):
             b")"
             b" && ARRAY[0, 1, 2]",
             self.qbe.sql(dict(x=[0,1,2])))
+        self.assertEqual(
+            b"array("
+            b"select value::int from jsonb_array_elements_text("
+            b"state->0-> 'z' "
+            b")"
+            b")",
+            self.qbe['x'].order_by(self.cursor, None))
+
+    def test_prefix(self):
+        from newt.qbe import prefix
+
+        self.qbe['x'] = prefix('x')
+        self.conn.commit()
+        self.assertEqual(b"((state ->> 'x') like 'y' || '%')",
+                         self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(state ->> 'x')",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = prefix('x', delimiter='/')
+        self.conn.commit()
+        self.assertEqual(b"((state ->> 'x') like 'y' || '/%')",
+                         self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(state ->> 'x')",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = prefix("state -> 0 -> 'x'")
+        self.conn.commit()
+        self.assertEqual(b"((state -> 0 ->> 'x') like 'y' || '%')",
+                         self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(state -> 0 ->> 'x')",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = prefix("path(state)")
+        self.conn.commit()
+        self.assertEqual(b"((path(state)) like 'y' || '%')",
+                         self.qbe.sql(dict(x='y')))
+        self.assertEqual(b"(path(state))",
+                         self.qbe['x'].order_by(self.cursor, 'y'))
+
+    def test_fulltext(self):
+        from newt.qbe import fulltext
+
+        self.qbe['x'] = fulltext('x')
+        self.conn.commit()
+        self.assertEqual(b"to_tsvector(state ->> 'x') @@ to_tsquery('y')",
+                         self.qbe.sql(dict(x='y')))
+        self.assertEqual(
+            b"ts_rank_cd({0.1, 0.2, 0.4, 1},"
+            b" to_tsvector(state ->> 'x'),"
+            b" to_tsquery('y'))",
+            self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = fulltext('x', config='klingon')
+        self.conn.commit()
+        self.assertEqual(
+            b"to_tsvector('klingon', state ->> 'x') @@"
+            b" to_tsquery('klingon', 'y')",
+            self.qbe.sql(dict(x='y')))
+        self.assertEqual(
+            b"ts_rank_cd({0.1, 0.2, 0.4, 1},"
+            b" to_tsvector('klingon', state ->> 'x'),"
+            b" to_tsquery('klingon', 'y'))",
+            self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = fulltext('x', config='klingon',
+                                 weights=(.2, .3, .5, .7))
+        self.conn.commit()
+        self.assertEqual(
+            b"ts_rank_cd({0.2, 0.3, 0.5, 0.7},"
+            b" to_tsvector('klingon', state ->> 'x'),"
+            b" to_tsquery('klingon', 'y'))",
+            self.qbe['x'].order_by(self.cursor, 'y'))
+
+        self.qbe['x'] = fulltext('x', parser=crazy_parse)
+        self.conn.commit()
+        self.assertEqual(b"to_tsvector(state ->> 'x') @@ to_tsquery('CRAZY y')",
+                         self.qbe.sql(dict(x='y')))
+
+    def test_qbe(self):
+        from newt.qbe import prefix, array, fulltext
+
+        self.qbe['path'] = prefix('path(state)', delimiter='/')
+        self.qbe['can_view'] = array('can_view(state)')
+        self.qbe['text'] = fulltext('text')
+        self.conn.commit()
+
+        self.assertEqual(
+            b"(can_view(state)) && ARRAY['p1', 'g2'] AND"
+            b" ((path(state)) like '/foo' || '/%') AND"
+            b" to_tsvector(state ->> 'text') @@ to_tsquery('thursday')"
+            b" ORDER BY ts_rank_cd({0.1, 0.2, 0.4, 1},"
+            b" to_tsvector(state ->> 'text'),"
+            b" to_tsquery('thursday'))",
+            self.qbe.sql(dict(path="/foo",
+                              can_view=['p1', 'g2'],
+                              text="thursday"),
+                         order_by='text'),
+            )
+
+        self.assertEqual(
+            b"(can_view(state)) && ARRAY['p1', 'g2'] AND"
+            b" ((path(state)) like '/foo' || '/%') AND"
+            b" to_tsvector(state ->> 'text') @@ to_tsquery('thursday')"
+            b" ORDER BY ts_rank_cd({0.1, 0.2, 0.4, 1},"
+            b" to_tsvector(state ->> 'text'),"
+            b" to_tsquery('thursday')) DESC",
+            self.qbe.sql(dict(path="/foo",
+                              can_view=['p1', 'g2'],
+                              text="thursday"),
+                         order_by='text', desc=True),
+            )
+
+def crazy_parse(q):
+    return 'CRAZY ' + q
